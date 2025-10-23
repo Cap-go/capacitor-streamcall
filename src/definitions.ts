fa321413ccccf7d58ef518bc4c8e2bc785c9cc19
Martin Donadieu <martindonadieu@gmail.com>
2025-10-23 06:20:35 +0100

    fix: definition

--- src/definitions.ts
+++ src/definitions.ts
@@ -434,6 +434,14 @@ export interface StreamCallPlugin {
    * console.log(currentUser);
    */
   getCurrentUser(): Promise<CurrentUserResponse>;
+
+    /**
+     * Get the native Capacitor plugin version
+     *
+     * @returns {Promise<{ id: string }>} an Promise with version for this device
+     * @throws An error if the something went wrong
+     */
+    getPluginVersion(): Promise<{ version: string }>;
 }
 
 /**
