2408e2404800c919ef64468367b0ba735e5e1e91
Martin Donadieu <martindonadieu@gmail.com>
2025-10-23 06:44:19 +0100

    feat: add getPluginVersion method to StreamCallPlugin

--- android/src/main/java/ee/forgr/capacitor/streamcall/StreamCallPlugin.kt
+++ android/src/main/java/ee/forgr/capacitor/streamcall/StreamCallPlugin.kt
@@ -107,6 +107,7 @@ import kotlinx.coroutines.flow.collectLatest
 // It's a spaghetti-like, tangled, unreadable mess and frankly, I am deeply sorry for the code crimes commited in the Android impl
 @CapacitorPlugin(name = "StreamCall")
 class StreamCallPlugin : Plugin() {
+  private val PLUGIN_VERSION = "7.5.0"
   private var streamVideoClient: StreamVideo? = null
   private var state: State = State.NOT_INITIALIZED
   private var overlayView: ComposeView? = null
@@ -3254,6 +3255,17 @@ class StreamCallPlugin : Plugin() {
       audioOnlyValue?.toString() == "true"
     } else {
       false
+    }
+  }
+
+  @PluginMethod
+  fun getPluginVersion(call: PluginCall) {
+    try {
+      val ret = JSObject()
+      ret.put("version", PLUGIN_VERSION)
+      call.resolve(ret)
+    } catch (e: Exception) {
+      call.reject("Could not get plugin version", e)
     }
   }
 }
