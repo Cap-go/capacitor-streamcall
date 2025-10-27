76f2db791382c1051e49f2f30596b86d256af583
Martin Donadieu <martindonadieu@gmail.com>
2025-10-27 03:43:50 +0000

    fix: remove old edge to egde

--- example-app/capacitor.config.ts
+++ example-app/capacitor.config.ts
@@ -4,6 +4,9 @@ const config: CapacitorConfig = {
   appId: 'app.capgo.streamcall',
   appName: 'Streamcall Example',
   webDir: 'www',
+  android: {
+    adjustMarginsForEdgeToEdge: 'auto',
+  },
   "plugins": {
     "Keyboard": {
       "resize": "body",
